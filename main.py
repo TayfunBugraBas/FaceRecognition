2387c55a5c139756cd1234c7f60567b250ee8b92
TayfunBugraBas <73062585+TayfunBugraBas@users.noreply.github.com>
2022-05-05 22:14:59 +0300

    .

--- main.py
+++ main.py
@@ -8,15 +8,24 @@ window = Tk()
 
 
 
-window.title("deneme")    
+window.title("deneme")  
+canvas = Canvas(window,height=450,width=750)  
+frameA = Frame(window,bg='#add8e6')
+frameB = Frame(window,bg='#add8e6')
+frameC = Frame(window,bg='#add8e6')
 
   
   
 def mainMenu():
-      window.geometry("800x600")
       
-      btn = Button(window,text="test", command = AppMain.faceRecognizerOpener)
+      
+      btn = Button(frameB,text="test", command = AppMain.faceRecognizerOpener)
+      canvas.pack()
       btn.pack()
+      frameA.place(relx=0.05,rely=0.01, relwidth=0.9,relheight=0.2)
+      frameB.place(relx=0.05,rely=0.25, relwidth=0.3,relheight=0.7)
+      frameC.place(relx=0.37,rely=0.25, relwidth=0.58,relheight=0.7)
+     
       
 
 mainMenu()
